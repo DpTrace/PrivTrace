3e54436b75bc03907ddb0fb12a9765d9fb882a3a
haiming-wang <96150403+haiming-wang@users.noreply.github.com>
2022-12-08 23:48:24 +0800

    Delete preparation.py

--- preparation.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-from data_preparation.trajectory_preparation import TrajectoryPreparation
-from config.parameter_setter import ParSetter
-
-if __name__ == "__main__":
-    args = ParSetter().set_up_args(epsilon=1)
-    trapre1 = TrajectoryPreparation()
-    trapre1.get_trajectory(args)
