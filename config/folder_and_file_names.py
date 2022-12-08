f6ee77d3db33d0e335d9901666b626a2ae20b09b
haiming-wang <96150403+haiming-wang@users.noreply.github.com>
2022-12-09 00:09:26 +0800

    Add files via upload

--- config/folder_and_file_names.py
+++ config/folder_and_file_names.py
@@ -1,4 +1,7 @@
-trajectory_data_folder = 'datasets'
+# the name of the input file
 dataset_file_name = 'simple_example.dat'
+# the name of the output file
 result_file_name = 'generated_tras.txt'
+
+trajectory_data_folder = 'datasets'
 
