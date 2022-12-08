dc9c7d331d2e5e13d70f8d8afa58dc5dd7c4846f
haiming-wang <96150403+haiming-wang@users.noreply.github.com>
2022-12-08 23:31:17 +0800

    Add files via upload

--- config/experiment_parameter.py
+++ config/experiment_parameter.py
@@ -1,15 +1,11 @@
-import numpy as np
-import os
 import argparse
 
 
 class ExperimentPar:
 
-    #
     def __init__(self):
         pass
 
-    #
     def set_experiment_par(self):
         parser = argparse.ArgumentParser()
 
@@ -17,8 +13,6 @@ class ExperimentPar:
         # parameter will be calculated
         parser.add_argument('--transition_error_step', type=float, default=5)
         parser.add_argument('--length_error_experiment_bin_number', type=float, default=20)
-
         parser.add_argument('--trip_error_grid_bins', type=float, default=500)
-
         args = vars(parser.parse_args())
         return args
